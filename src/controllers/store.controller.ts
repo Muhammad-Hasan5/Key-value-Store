e625f5ef132200e9b7dca51189170ce2faf20937
Muhammad Hasan <hasanamir8901@gmail.com>
2026-04-07 04:32:35 +0500

    add file storage in controller layer

--- src/controllers/store.controller.ts
+++ src/controllers/store.controller.ts
@@ -1,31 +1,41 @@
 import type { Request, Response } from "express";
 import store from "../services/store.service.js";
+import { appendLog } from "../utils/wal.js";
 
 type Params = {
-    key: string
-}
+  key: string;
+};
 
 export async function set(req: Request, res: Response) {
-  const { key, value } = req.body || {};
+  try {
+    const { key, value } = req.body || {};
 
-  if (key === undefined || value === undefined) {
-    return res
-      .status(400)
-      .json({ status: 400, message: "key string and its value are required" });
+    if (key === undefined || value === undefined) {
+      return res
+        .status(400)
+        .json({
+          status: 400,
+          message: "key string and its value are required",
+        });
+    }
+
+    if (key === "" || value === "") {
+      return res
+        .status(400)
+        .json({ status: 400, message: "key and value string cannot be empty" });
+    }
+
+    store.set(key, value);
+    await appendLog({method: "PUT", key, value});
+
+    return res.status(201).json({
+      status: 201,
+      message: "key and value stored successfully",
+    });
+  } catch (error: any) {
+    if (error.code === "ENOENT") return;
+    throw error;
   }
-
-  if (key === "" || value === "") {
-    return res
-      .status(400)
-      .json({ status: 400, message: "key and value string cannot be empty" });
-  }
-
-  store.set(key, value);
-
-  return res.status(201).json({
-    status: 201,
-    message: "key and value stored successfully",
-  });
 }
 
 export async function get(req: Request<Params>, res: Response) {
@@ -44,7 +54,9 @@ export async function get(req: Request<Params>, res: Response) {
   const value = store.get(key);
 
   if (value === undefined) {
-    return res.status(404).json({ status: 404, message: "key not found", data: null });
+    return res
+      .status(404)
+      .json({ status: 404, message: "key not found", data: null });
   }
 
   res.status(200).json({
@@ -55,27 +67,33 @@ export async function get(req: Request<Params>, res: Response) {
 }
 
 export async function remove(req: Request<Params>, res: Response) {
-  const key = req.params.key;
+  try {
+    const key = req.params.key;
 
-  if (!key) {
-    return res.status(400).json({ status: 400, message: "key is required" });
+    if (!key) {
+      return res.status(400).json({ status: 400, message: "key is required" });
+    }
+
+    if (key === "") {
+      return res
+        .status(400)
+        .json({ status: 400, message: "key cannot be empty" });
+    }
+
+    if (!store.has(key)) {
+      return res.status(404).json({ status: 404, message: "key not found" });
+    }
+
+    store.remove(key);
+    await appendLog({ method: "DELETE", key });
+
+    res.status(200).json({
+      status: 200,
+      message: "key removed successfully",
+      data: null,
+    });
+  } catch (error: any) {
+    if (error.code === "ENOENT") return;
+    throw error;
   }
-
-  if (key === "") {
-    return res
-      .status(400)
-      .json({ status: 400, message: "key cannot be empty" });
-  }
-
-  if (!store.has(key)) {
-    return res.status(404).json({ status: 404, message: "key not found" });
-  }
-
-  store.remove(key);
-
-  res.status(200).json({
-    status: 200,
-    message: "key removed successfullt",
-    data: null,
-  });
 }
