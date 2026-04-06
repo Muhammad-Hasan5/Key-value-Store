ef301ee6e119d5851914f223df0826cf31106083
Muhammad Hasan <hasanamir8901@gmail.com>
2026-04-07 04:30:28 +0500

    improve routing params

--- dist/routes/store.route.js
+++ dist/routes/store.route.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
-import { set, get, getStore, remove } from "../controllers/store.controller.js";
+import { set, get, remove } from "../controllers/store.controller.js";
 const router = Router();
-router.route("/store").put(set).get(get).delete(remove);
-router.route("/get-store").get(getStore);
+router.route("/store").put(set);
+router.route("/store/:key").get(get).delete(remove);
 export default router;
 //# sourceMappingURL=store.route.js.map
