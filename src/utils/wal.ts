526067fad1da51489f7549285b518829f1404339
Muhammad Hasan <hasanamir8901@gmail.com>
2026-04-07 15:35:34 +0500

    add queue and batch processing of the pairs

--- src/utils/wal.ts
+++ src/utils/wal.ts
@@ -4,42 +4,69 @@ import fs from "node:fs/promises";
 import path from "node:path";
 
 interface pair {
-  method: string;
+  method: "PUT" | "DELETE";
   key: string;
   value?: string;
 }
 
-const LOG_FILE = path.join(process.cwd(), "store.json");
+class WAL {
+  private queue: pair[] = [];
+  private isFlushing: boolean = false;
+  private LOG_FILE = path.join(process.cwd(), "store.json");
 
-export async function appendLog(entry: pair) {
-  try {
-    await fs.appendFile(LOG_FILE, JSON.stringify(entry) + "\n");
-  } catch (error: any) {
-    if (error.code === "ENOENT") return;
-    throw error;
+  constructor() {
+    setInterval(() => this.flush(), 10);
   }
-}
 
-export async function recovery(store: Map<string, string>) {
-  try {
-    const data = await fs.readFile(LOG_FILE, { encoding: "utf-8" });
-    const lines = data.split("\n");
+  private async flush() {
+    if (this.isFlushing) return;
+    if (this.queue.length === 0) return;
 
-    for (const line of lines) {
-      if (!line) continue;
+    this.isFlushing = true;
 
-      const pair: pair = JSON.parse(line);
+    const batch = this.queue.splice(0, this.queue.length);
 
-      if (pair.method === "PUT") {
-        store.set(pair.key as string, pair.value as string);
-      }
+    const data = batch.map((p) => JSON.stringify(p)).join("\n") + "\n";
 
-      if (pair.method === "DELETE") {
-        store.delete(pair.key as string);
-      }
+    try {
+      await fs.appendFile(this.LOG_FILE, data);
+    } catch (error: any) {
+      if (error.code === "ENOENT") return;
+      throw error;
+    } finally {
+      this.isFlushing = false;
     }
-  } catch (error: any) {
-    if (error.code === "ENOENT") return;
-    throw error;
+  }
+
+  async appendLog(entry: pair) {
+    this.queue.push(entry);
+  }
+
+  async recovery(store: Map<string, string>) {
+    try {
+      const data = await fs.readFile(this.LOG_FILE, { encoding: "utf-8" });
+      const lines = data.split("\n");
+
+      for (const line of lines) {
+        if (!line) continue;
+
+        const pair: pair = JSON.parse(line);
+
+        if (pair.method === "PUT") {
+          store.set(pair.key as string, pair.value as string);
+        }
+
+        if (pair.method === "DELETE") {
+          store.delete(pair.key as string);
+        }
+      }
+    } catch (error: any) {
+      if (error.code === "ENOENT") return;
+      throw error;
+    }
   }
 }
+
+const wal = new WAL();
+
+export default wal;
