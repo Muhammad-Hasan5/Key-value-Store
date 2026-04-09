1650a704d3b43e3ea4aed39b8ea79cdd9aa9cff5
Muhammad Hasan <hasanamir8901@gmail.com>
2026-04-09 23:56:05 +0500

    create http server for store

---
+++ dist/http.d.ts
@@ -0,0 +1,4 @@
+import express from "express";
+declare const http: express.Application;
+export default http;
+//# sourceMappingURL=http.d.ts.map
---
+++ src/http.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import router from "./routes/store.route.js";
+
+const http: express.Application = express();
+
+http.use(express.json({limit: "5mb"}));
+http.use(express.urlencoded({ limit: "5mb", extended: true }));
+
+http.use("/", router);
+
+http.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+export default http;
